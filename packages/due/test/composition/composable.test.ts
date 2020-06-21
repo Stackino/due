6e2bf8a569b1f6486e2a2ee845fd2b5bbfd37ab4
Lukáš Novotný <lukas@dramiel.com>
2020-06-21 15:45:01 +0200

    update deps

--- packages/due/test/composition/composable.test.ts
+++ packages/due/test/composition/composable.test.ts
@@ -6,97 +6,97 @@ import { observable, runInAction } from 'mobx';
 
 class MockComposable extends Composable {
 
-    report: string[] = [];
+	report: string[] = [];
 
-    timer = this.$timer(250, async () => {
-        this.report.push('timer tick');
-    });
+	timer = this.$timer(250, async () => {
+		this.report.push('timer tick');
+	});
 
-    @observable
-    reactionTrigger = '';
+	@observable
+	reactionTrigger = '';
 
-    reaction = this.$reaction(() => this.reactionTrigger, data => {
-        this.report.push(`reaction tick ${data}`);
-    });
+	reaction = this.$reaction(() => this.reactionTrigger, data => {
+		this.report.push(`reaction tick ${data}`);
+	});
 
 }
 
 async function setup(): Promise<[Container]> {
-    const container = new DefaultContainer();
-    container.bindConstantValue(ContainerTag, container);
-    container.bind(DiagnosticsServiceTag, DefaultDiagnosticsService, BindingScope.singleton);
+	const container = new DefaultContainer();
+	container.bindConstantValue(ContainerTag, container);
+	container.bind(DiagnosticsServiceTag, DefaultDiagnosticsService, BindingScope.singleton);
 
-    return [container];
+	return [container];
 }
 
 test('timer is disabled by default', async () => {
-    const [container] = await setup();
+	const [container] = await setup();
 
-    const composable = container.instantiate(MockComposable);
-    expect(composable.timer.enabled).toBe(false);
-    expect(composable.timer.running).toBe(false);
+	const composable = container.instantiate(MockComposable);
+	expect(composable.timer.enabled).toBe(false);
+	expect(composable.timer.running).toBe(false);
 
-    expect(composable.report.length).toBeLessThanOrEqual(0);
+	expect(composable.report.length).toBeLessThanOrEqual(0);
 });
 
 test('timer is ticking', async () => {
-    const [container] = await setup();
+	const [container] = await setup();
 
-    const composable = container.instantiate(MockComposable);
-    expect(composable.timer.enabled).toBe(false);
-    expect(composable.timer.running).toBe(false);
+	const composable = container.instantiate(MockComposable);
+	expect(composable.timer.enabled).toBe(false);
+	expect(composable.timer.running).toBe(false);
 
-    composable.timer.enable();
-    expect(composable.timer.enabled).toBe(true);
-    expect(composable.timer.running).toBe(false);
+	composable.timer.enable();
+	expect(composable.timer.enabled).toBe(true);
+	expect(composable.timer.running).toBe(false);
 
-    await delay(600);
+	await delay(600);
 
-    composable.timer.disable();
-    expect(composable.timer.enabled).toBe(false);
-    expect(composable.timer.running).toBe(false);
+	composable.timer.disable();
+	expect(composable.timer.enabled).toBe(false);
+	expect(composable.timer.running).toBe(false);
 
-    expect(composable.report.length).toBe(2);
-    expect(composable.report[0]).toBe('timer tick');
-    expect(composable.report[1]).toBe('timer tick');
+	expect(composable.report.length).toBe(2);
+	expect(composable.report[0]).toBe('timer tick');
+	expect(composable.report[1]).toBe('timer tick');
 });
 
 test('reaction is disabled by default', async () => {
-    const [container] = await setup();
+	const [container] = await setup();
 
-    const composable = container.instantiate(MockComposable);
-    expect(composable.reaction.enabled).toBe(false);
-    expect(composable.reaction.running).toBe(false);
+	const composable = container.instantiate(MockComposable);
+	expect(composable.reaction.enabled).toBe(false);
+	expect(composable.reaction.running).toBe(false);
 
-    expect(composable.report.length).toBeLessThanOrEqual(0);
+	expect(composable.report.length).toBeLessThanOrEqual(0);
 });
 
 test('reaction is reacting', async () => {
-    const [container] = await setup();
+	const [container] = await setup();
 
-    const composable = container.instantiate(MockComposable);
-    expect(composable.reaction.enabled).toBe(false);
-    expect(composable.reaction.running).toBe(false);
+	const composable = container.instantiate(MockComposable);
+	expect(composable.reaction.enabled).toBe(false);
+	expect(composable.reaction.running).toBe(false);
 
-    composable.reaction.enable();
-    expect(composable.reaction.enabled).toBe(true);
-    expect(composable.reaction.running).toBe(false);
+	composable.reaction.enable();
+	expect(composable.reaction.enabled).toBe(true);
+	expect(composable.reaction.running).toBe(false);
 
-    composable.reactionTrigger = 'foo';
+	composable.reactionTrigger = 'foo';
 
-    runInAction(() => {
-        composable.reactionTrigger = 'baz';
-        composable.reactionTrigger = 'bar';
-    });
+	runInAction(() => {
+		composable.reactionTrigger = 'baz';
+		composable.reactionTrigger = 'bar';
+	});
 
-    // execution is delayed
-    await delay(10);
+	// execution is delayed
+	await delay(10);
 
-    composable.reaction.disable();
-    expect(composable.reaction.enabled).toBe(false);
-    expect(composable.reaction.running).toBe(false);
+	composable.reaction.disable();
+	expect(composable.reaction.enabled).toBe(false);
+	expect(composable.reaction.running).toBe(false);
 
-    expect(composable.report.length).toBe(2);
-    expect(composable.report[0]).toBe('reaction tick foo');
-    expect(composable.report[1]).toBe('reaction tick bar');
+	expect(composable.report.length).toBe(2);
+	expect(composable.report[0]).toBe('reaction tick foo');
+	expect(composable.report[1]).toBe('reaction tick bar');
 });
--- packages/due/test/ioc/container.test.ts
+++ packages/due/test/ioc/container.test.ts
@@ -13,42 +13,42 @@ const Dependency2Tag = new Tag<Dependency2>('Test dependency 2');
 class Dependency2 {}
 
 class ConsumerBase {
-    @inject(Dependency1Tag)
-    public dependency1!: Dependency1;
+	@inject(Dependency1Tag)
+	public dependency1!: Dependency1;
 }
 
 class Consumer extends ConsumerBase {
-    @inject(Dependency2Tag)
-    public dependency2!: Dependency2;
+	@inject(Dependency2Tag)
+	public dependency2!: Dependency2;
 }
 
 class ObjectConsumerBase extends Injectable {
-    public dependency1 = this.$dependency(Dependency1Tag);
+	public dependency1 = this.$dependency(Dependency1Tag);
 }
 
 class ObjectConsumer extends Injectable {
-    public dependency2 = this.$dependency(Dependency2Tag);
+	public dependency2 = this.$dependency(Dependency2Tag);
 }
 
 test('decorators: resolves class hierarchy', async () => {
-    const container = new DefaultContainer();
-    container.bind(Dependency1Tag, Dependency1, BindingScope.singleton);
-    container.bind(Dependency2Tag, Dependency2, BindingScope.singleton);
+	const container = new DefaultContainer();
+	container.bind(Dependency1Tag, Dependency1, BindingScope.singleton);
+	container.bind(Dependency2Tag, Dependency2, BindingScope.singleton);
 
-    const consumer = new Consumer();
-    container.inject(consumer);
+	const consumer = new Consumer();
+	container.inject(consumer);
 
-    expect(consumer.dependency1 instanceof Dependency1).toBe(true);
-    expect(consumer.dependency2 instanceof Dependency2).toBe(true);
+	expect(consumer.dependency1 instanceof Dependency1).toBe(true);
+	expect(consumer.dependency2 instanceof Dependency2).toBe(true);
 });
 
 test('base object: resolves class hierarchy', async () => {
-    const container = new DefaultContainer();
-    container.bind(Dependency1Tag, Dependency1, BindingScope.singleton);
-    container.bind(Dependency2Tag, Dependency2, BindingScope.singleton);
+	const container = new DefaultContainer();
+	container.bind(Dependency1Tag, Dependency1, BindingScope.singleton);
+	container.bind(Dependency2Tag, Dependency2, BindingScope.singleton);
 
-    const consumer = container.instantiate(Consumer);
+	const consumer = container.instantiate(Consumer);
 
-    expect(consumer.dependency1 instanceof Dependency1).toBe(true);
-    expect(consumer.dependency2 instanceof Dependency2).toBe(true);
+	expect(consumer.dependency1 instanceof Dependency1).toBe(true);
+	expect(consumer.dependency2 instanceof Dependency2).toBe(true);
 });

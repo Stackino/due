34cd463773d6224da53dd2153b3d6569ba426b61
Lukáš Novotný <lukas@dramiel.com>
2022-04-03 15:16:26 +0200

    fix cross package references

--- packages/due-plugin-react/src/plugin.ts
+++ packages/due-plugin-react/src/plugin.ts
@@ -1,38 +1,23 @@
 import { Plugin, RenderServiceTag, ServiceCollection } from '@stackino/due';
+import React from 'react';
 import { ReactRenderService, ReactRenderServiceOptions, ReactRenderServiceOptionsTag } from './render-service';
 
 export interface ReactPluginOptions {
-	output: string | HTMLElement | ((html: string) => void);
+	render: (app: React.ReactNode) => void;
 }
 
 export class ReactPlugin extends Plugin {
 	constructor(options: ReactPluginOptions) {
 		super();
+
+		const render = options.render;
 		
-		let output = options.output;
-
-		if (typeof output === 'string') {
-			const foundContainers = document.querySelectorAll(output);
-			if (foundContainers.length !== 1) {
-				throw new Error('Exactly one container element is required');
-			}
-
-			const foundContainer = foundContainers.item(0);
-			if (!(foundContainer instanceof HTMLElement)) {
-				throw new Error('Exactly one container element of type HTMLElement is required');
-			}
-
-			output = foundContainer;
-		} else if (typeof output !== 'function' && !(output instanceof HTMLElement)) {
-			throw new Error('Invalid output. It can be ID of existing DOM node, DOM node or function accepting html (server rendering).');
-		}
-
-		if (!output) {
-			throw new Error('Output is required. It can be ID of existing DOM node, DOM node or function accepting html (server rendering).');
+		if (!render) {
+			throw new Error('Render is required. It must be a function accepting React.ReactNode.');
 		}
 
 		this.options = {
-			output,
+			render,
 		};
 	}
 
